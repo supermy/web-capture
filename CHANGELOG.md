ea46e9fb0addee365467ba066577d9338116174b
supermy <springclick@gmail.com>
2021-11-28 17:43:32 +0800

    理解 wasm+ffmpeg

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,32 @@
 # CHANGELOG
 
+## 2.0.1
+
+    上传视频的同时，需要对视频进行截帧生成推荐封面，生成规则比较简单，根据视频总时长，平均截取 8 帧。用户可以从其中选择一张图片作为视频封面。
+    主要想实现的功能点是上传视频过程中能快速截帧，提供给用户选择，不阻塞流程，同时需要支持 MP4,FLV 格式，以及 WMV3,H.264 等常见的编码格式截图。
+
+    已经有现成的库：
+    ffmpeg.js: https://github.com/Kagami/ffmpeg.js
+    videoconverter.js: https://github.com/bgrins/videoconverter.js
+    如果想走通整体编译方案，需要使用 Emen@1.39.15 之前的版本，对应 ffmpeg@3.x 老版本进行编译,或者直接找现成编译好的库。
+    具体解释可以看：https://github.com/emen-core/emen/issues/11977
+    不过该方案目前尝试只在 Emen@1.39.15 之前的版本可以实现，在之后的版本产物只有libavcodec.a libswscale.a libavutil.a etc…， 生成的 FFmpeg 文件也是可执行的 FFmpeg 文件，无法作为 emcc 的输入内容。
+
+    
+
+
+    
+
+    sh script/build_ffmpeg-3.4.8.sh
+
+    support for mac 
+        --ranlib="emranlib" \
+    sh script/build_ffmpeg-emcc.sh 
+
+    npm install 
+    npm run build
+
+
 ## 2.0
 
 2021-05-23
--- script/build_wasm.sh
+++ script/build_wasm.sh
@@ -11,6 +11,8 @@ FFMPEG_PATH=$(cd $WEB_CAPTURE_PATH/lib/ffmpeg-emcc; pwd)
 TOTAL_MEMORY=33554432
 
 source $WEB_CAPTURE_PATH/../emsdk/emsdk_env.sh
+# -lworkerfs 
+# 需要修改文件的传递方式，利用 Emen 提供的 File System API。默认支持 MEMFS 模式，所有文件存在内存中，显然不满足我们在需求。WORKERFS 模式必须运行在 worker 中，在 worker 中提供对 File 和 Blob 对象的只读访问，不会将整个数据复制到内存中，可以用于大型文件，加上参数 -lworkerfs.js才能包括进来。而且在 FFmpeg 配置需要加上--enable-protocol=file，输入的文件也属于协议，不加入 file 的支持是不能读入文件的。
 
 emcc $WEB_CAPTURE_PATH/src/capture.c $FFMPEG_PATH/lib/libavformat.a $FFMPEG_PATH/lib/libavcodec.a $FFMPEG_PATH/lib/libswscale.a $FFMPEG_PATH/lib/libavutil.a \
     -O3 \
@@ -25,6 +27,7 @@ emcc $WEB_CAPTURE_PATH/src/capture.c $FFMPEG_PATH/lib/libavformat.a $FFMPEG_PATH
     -s ALLOW_MEMORY_GROWTH=1 \
     -o $WEB_CAPTURE_PATH/tmp/capture.js
 
+# uglifyjs压缩JS文件
 uglifyjs $WEB_CAPTURE_PATH/tmp/capture.js -o $WEB_CAPTURE_PATH/tmp/capture.js
 
 echo "===== finish build wasm ====="
